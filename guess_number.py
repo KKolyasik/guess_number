111359c81a8abebd79056ae8d6d91757cc6322b4
Klimko Nikolay <klimko.nikolya@gmail.com>
2024-10-07 13:06:35 +0300

    optimize key logic

--- guess_number.py
+++ guess_number.py
@@ -2,19 +2,19 @@ from random import randint
 number = randint(1, 100)
 print('Угадайте число от 1 до 100')
 
-def guess_number():
+def guess_wow_wow():
 
     while True:
-        guess = int(input(' Ввидите число: '))
+        guess_wow = int(input(' Ввидите число: '))
 
-        if guess < number:
+        if guess_wow < number:
             print('Ваше число меньше загаданного')
 
-        elif guess > number:
+        elif guess_wow > number:
             print('Ваше число больше загаданного')
 
         else:
             break
 
     print('Отличная интуиция! Вы угадали число:)')  
-guess_number()
+guess_wow_wow()
